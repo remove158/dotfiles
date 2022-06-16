4946110094f8aea8b92b3006cb9f4488f406e55d
Piyaphat Pinyo <p.piyaphat@ext-lmwn.com>
2022-06-16 16:42:27 +0700

    :necktie: add: oh-my-zsh plugin

---
+++ setup/zsh/01-install-oh-my-zsh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if ! [ -d ~/.oh-my-zsh ]
+then
+  echo "🟣 installing oh-my-zsh."
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "✅ oh-my-zsh installed!"
+else
+  echo "🟣 oh-my-zsh is installed."
+fi
