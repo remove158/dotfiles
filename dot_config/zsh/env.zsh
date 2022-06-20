8e83fa1c4bb20dbc0abfcd67db262a91d85a4e1c
Piyaphat Pinyo <remove158@gmail.com>
2022-06-20 12:10:22 +0700

    :necktie: add: go bin

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -1,2 +1,8 @@
+# system
+export PATH=$HOME/bin:/usr/local/bin:.:$PATH:~/go/bin
+export PNPM_HOME="/Users/osm/Library/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+# local dev
 export ENV=beta
-export PATH=$HOME/bin:/usr/local/bin:.:$PATH
+
